72ac30da7708b8090d1f2ebcc8f97863556f8a83
menox145 <anvikalputra@gmail.com>
2024-01-17 15:16:25 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
     <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae voluptates itaque ratione assumenda, amet fuga ex ea laborum, architecto sit nemo id aspernatur fugit perferendis inventore quisquam illo culpa omnis.
     </p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
